54a0e2c7982e6fc1ad0e23b25d9a6359cecb4378
AlihanCelik <alihan_08032003@hotmail.com>
2024-07-07 13:29:55 +0300

    moviesApp

--- app/src/main/java/com/example/moviesapp/LoginActivity.kt
+++ app/src/main/java/com/example/moviesapp/LoginActivity.kt
@@ -1,20 +1,19 @@
 package com.example.moviesapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.moviesapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.loginBtn.setOnClickListener {
+            val intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
